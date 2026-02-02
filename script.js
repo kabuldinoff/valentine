fa380f07b8616b5269bcfbb95e2a4b822f86a4a9
Alibek Kabuldinov <alibek99.99@mail.ru>
2026-02-02 16:16:27 +0100

    Add 2 new pages and CSS fireworks
    * Optimize logic and add new images

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ const yesBtn = document.querySelector('.btn-yes');
 const noBtn = document.querySelector('.btn-no');
 let currentYesSize = 1;
 
-noBtn.addEventListener( 'mouseover', () => {
-    const x = Math.random() * (window.innerWidth - 100);
-    const y = Math.random() * (window.innerHeight - 50);
+yesBtn.addEventListener('click', () => {
+    window.location.href = 'yes.html';
+});
 
-    noBtn.style.position = 'absolute';
-    noBtn.style.left = `${x}px`;
-    noBtn.style.top = `${y}px`;
+noBtn.addEventListener( 'mouseover', () => {
+    const btnWidth = noBtn.offsetWidth;
+    const btnHeight = noBtn.offsetHeight;
+    const maxWidth = window.innerWidth - btnWidth;
+    const maxHeight = window.innerHeight - btnHeight;
+    const randomX = Math.floor(Math.random() * maxWidth);
+    const randomY = Math.floor(Math.random() * maxHeight);
+
+    noBtn.style.position = 'fixed';
+    noBtn.style.zIndex = '100';
+    noBtn.style.left = randomX + 'px';
+    noBtn.style.top = randomY + 'px';
     currentYesSize = currentYesSize * 1.2;
     yesBtn.style.transform = `scale(${currentYesSize})`;
 });
